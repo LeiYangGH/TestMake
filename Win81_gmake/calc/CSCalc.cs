7241b75e6f199a81f2497467f8c4a8c1a193ea08
Lei Yang <675686066@qq.com>
2016-12-09 15:53:15 +0800

    can ref dll by cs

--- Win81_gmake/calc/CSCalc.cs
+++ Win81_gmake/calc/CSCalc.cs
@@ -1,14 +1,16 @@
 using System;
-
+using System.Runtime.InteropServices;
 namespace Calc
 {
 	public class Program
 	{
+		[DllImport(@"sum.dll", CallingConvention = CallingConvention.Cdecl)]
+		static extern int sum(int a, int b);
 		public static void Main()
 		{
 			int a = 2;
 			int b = 3;
-			int c = a * b;
+			int c = sum(a, b);
 			Console.WriteLine("{0}*{1}={2}", a, b, c);
 			Console.ReadLine();
 		}
