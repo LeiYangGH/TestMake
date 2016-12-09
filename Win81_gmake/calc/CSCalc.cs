8e26f96ff1f5408b39a956729ebaf07f53eeff25
Lei Yang <675686066@qq.com>
2016-12-09 14:37:35 +0800

    CSCalc

---
+++ Win81_gmake/calc/CSCalc.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Calc
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			int a = 2;
+			int b = 3;
+			int c = a * b;
+			Console.WriteLine("{0}*{1}={2}", a, b, c);
+			Console.ReadLine();
+		}
+	}
+}
