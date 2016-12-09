e71e857dc4fcd9f21fef01783ee5e6a835290c22
Lei Yang <675686066@qq.com>
2016-12-09 14:20:46 +0800

    calc by sum.h and sum.c

--- Win81_gmake/calc/calc.c
+++ Win81_gmake/calc/calc.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include"sum.h"
 int main()
 {
+	int a = 2;
+	int b = 3;
+	int c = sum(a, b);
+	printf("sum = %d\n",c);
+
 	printf("hello calc by make!\n");
 	system("pause");
 }
